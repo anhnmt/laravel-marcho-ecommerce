134f867547a73590eaa67d1dbc18669c6dc44464
doremon04 <62241285+Minh-wibu@users.noreply.github.com>
2020-07-22 13:50:29 +0700

    update role, ...

--- resources/views/backend/attribute/index.blade.php
+++ resources/views/backend/attribute/index.blade.php
@@ -19,11 +19,13 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="card">
+					@can('admin.attribute.create')
 					<div class="card-header">
 						<h3 class="card-title">
 							<a href="{{ route('admin.attribute.create') }}" class="btn btn-primary">Thêm thuộc tính</a>
 						</h3>
 					</div>
+					@endcan
 					<div class="card-body">
 						<div class="table-responsive">
 							<table id="datatables" class="table table-bordered table-hover">
