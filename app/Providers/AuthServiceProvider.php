0f1bad764f95bafb6231e43f8989bc73e846d055
doremon04 <62241285+doremon04@users.noreply.github.com>
2020-07-22 14:21:15 +0700

    Minh (#2) Update role, ...
    * Update Role, Permission, ...
    
    * update role, ...
    
    Co-authored-by: Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
    Co-authored-by: doremon04 <62241285+Minh-wibu@users.noreply.github.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,9 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         // Implicitly grant "Super Admin" role all permissions
         // This works in the app by using gate-related functions like auth()->user->can() and @can()
         Gate::before(function ($user, $ability) {
-            if ($user->hasRole('super-admin')) {
-                return true;
-            }
+            return $user->hasRole('super-admin') ? true : null;
         });
     }
 }
